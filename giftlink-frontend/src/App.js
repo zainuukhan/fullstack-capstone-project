e41b5405d166dda3fd7a16a7801e2032aa07560e
Zain Khan <136124108+zainuukhan@users.noreply.github.com>
2026-09-06 14:52:42 +0500

    ..

---
+++ giftlink-frontend/src/App.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './components/MainPage/MainPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/app" element={<MainPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
